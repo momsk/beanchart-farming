ac8db8f25263ae8b2403a2ddc0dd12d58af4d28f
makermakes <joshua@makercreative.com.au>
2022-01-26 14:39:32 +1100

    Frontend Build

---
+++ frontend/build/precache-manifest.dd405429ee99cbc6825c2f828fa6b445.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebc0639155350b25b45cd92c6ae8181c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a80f4985d0712e8c873",
+    "url": "/static/css/main.d5a7b4dd.chunk.css"
+  },
+  {
+    "revision": "7c1c7c4216f7caadeefd",
+    "url": "/static/js/0.069ea44a.chunk.js"
+  },
+  {
+    "revision": "7ba0a90e577e97cf5c04",
+    "url": "/static/js/10.0cb008e7.chunk.js"
+  },
+  {
+    "revision": "06684808af4c131220eb",
+    "url": "/static/js/11.e39089ba.chunk.js"
+  },
+  {
+    "revision": "50113376a820ff3bd6f4",
+    "url": "/static/js/3.c573fae1.chunk.js"
+  },
+  {
+    "revision": "f0d8f8707225ecf988339ffb06c72092",
+    "url": "/static/js/3.c573fae1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fdb6355118f51f85f81",
+    "url": "/static/js/4.d027d567.chunk.js"
+  },
+  {
+    "revision": "16ee8fb34f721263c47a",
+    "url": "/static/js/5.ee52a189.chunk.js"
+  },
+  {
+    "revision": "800c8c88b71490b35d9e636bbf8c583d",
+    "url": "/static/js/5.ee52a189.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6daf586a905b7ac0272",
+    "url": "/static/js/6.e6ec0a7a.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/static/js/6.e6ec0a7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bea71fea8e4799ea0ffb",
+    "url": "/static/js/7.e701f458.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/7.e701f458.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aaa3f95069a51eb9a3c",
+    "url": "/static/js/8.49bb6414.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/static/js/8.49bb6414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f85943e839b7f851f34",
+    "url": "/static/js/9.67fa125f.chunk.js"
+  },
+  {
+    "revision": "2a80f4985d0712e8c873",
+    "url": "/static/js/main.20c98d95.chunk.js"
+  },
+  {
+    "revision": "647b16b48f683ec309db",
+    "url": "/static/js/runtime-main.d0dd1a6e.js"
+  },
+  {
+    "revision": "7dd627e1e90d633de38b7b3d87d82ad9",
+    "url": "/static/media/logo-uniswap.7dd627e1.svg"
+  },
+  {
+    "revision": "e5956f843350d21224015f6756ebafaa",
+    "url": "/static/media/logo.e5956f84.png"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "/static/media/metamask-fox.c06f3a3e.svg"
+  }
+]);
---
+++ frontend/build/static/js/9.67fa125f.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpyam=this.webpackJsonpyam||[]).push([[9],{653:function(n,p){},655:function(n,p){}}]);
+//# sourceMappingURL=9.67fa125f.chunk.js.map
